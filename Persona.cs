fc5dc1a89ab4359855d79da447bc144751ef0d3b
beatriz <natsukibety22@gmail.com>
2026-09-15 12:55:00 -0700

    Agregar archivos de proyecto.

---
+++ Persona.cs
@@ -0,0 +1,19 @@
+﻿
+internal class Persona
+{
+    public string Name { get; set; }
+    public int Edad { get; set; }
+    public string ApellidoP { get; set; }
+    public string ApellidoM { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+
+    // Constructor de la clase Persona
+    public Persona(string name, int edad, string apellidoP, string apellidoM, DateTime fechaNacimiento)
+    {
+        Name = name;
+        Edad = edad;
+        ApellidoP = apellidoP;
+        ApellidoM = apellidoM;
+        FechaNacimiento = fechaNacimiento;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Instanciar la clase Persona y asignar valores a sus propiedades
+
+DateTime nacimiento = Convert.ToDateTime("1990-01-01");
+
+Persona persona = new Persona(
+    name: "Juan",
+    edad: 20,
+    apellidoP: "Pérez",
+    apellidoM: "García",
+    fechaNacimiento: nacimiento
+);
+
+
+DateTime nacimiento2 = Convert.ToDateTime("1995-05-15");
+
+Persona persona2 = new Persona(
+    name: "Maria",
+    edad: 33,
+    apellidoP: "López",
+    apellidoM: "Hernández",
+    fechaNacimiento: nacimiento2
+);
+
+
+// Mostrar la información de las personas
+
+Console.WriteLine(
+    $"La persona 1 es {persona.Name} {persona.ApellidoP} " +
+    $"{persona.ApellidoM}, tiene {persona.Edad} años y nació el " +
+    $"{persona.FechaNacimiento.ToShortDateString()}"
+);
+
+Console.WriteLine(
+    $"La persona 2 es {persona2.Name} {persona2.ApellidoP} " +
+    $"{persona2.ApellidoM}, tiene {persona2.Edad} años y nació el " +
+    $"{persona2.FechaNacimiento.ToShortDateString()}"
+);
